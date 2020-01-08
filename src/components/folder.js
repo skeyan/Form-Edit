e6a173286106bbae541e9f5861a027ceb387a607
Bobur <bobur.hu0991@gmail.com>
2020-01-08 14:51:09 -0500

    folder first commit

--- src/components/folder.js
+++ src/components/folder.js
@@ -1,5 +1,6 @@
 import React from "react";
 // import PropTypes from "prop-types";
+import { withStyles, Card, CardContent, Typography, Button } from '@material-ui/core/';
 
 class Folder extends React.Component {
     constructor() {
@@ -8,9 +9,23 @@ class Folder extends React.Component {
             toggle : false
         }
     }
+    handleToggle = () => {
+        this.state.toggle = !this.state.toggle;
+    }
     render() {
+        if(this.state.toggle){
+            <div>
+                <Typography variant="h5">Home</Typography>
+                <Button onClick={this.handleToggle}/>
+            </div>
+        }
         return(
-
+            <div>
+                <Typography variant="h5">Home</Typography>
+                <Typography variant="h5">File 1</Typography>
+                <Typography variant="h5">File 2</Typography>
+                <Typography variant="h5">File 3</Typography>
+            </div>
         );
     }
 
