dd0a91486bcaeb7e2c8eeb1bc6ad4834f706b70a
Eva Yan <eyan0749@gmail.com>
2020-01-08 13:23:26 -0500

    Added edit button

--- src/App.js
+++ src/App.js
@@ -3,16 +3,40 @@
   CUNY 2X BOOTCAMP
   JAN 8, 2020
 */
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import './App.css';
 
+class EditFormComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstname: props.firstname,
+      lastname: props.lastname
+    }
+  }
+
+  render() {
+    return(
+      <div> 
+        <div> </div> 
+        <div> </div>
+        <button>
+          <p>Edit</p>
+        </button>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div> </div>
+      <EditFormComponent firstname="first name" lastname="last name"/>
     );
   }
 }
+
+
 
 export default App;
